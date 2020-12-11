fa0226518d52cf076db9e595a5624d292d2c63a3
chubbytoey <toey.sasikrang@gmail.com>
2020-12-11 16:13:53 +0700

    EDIT chart from default data to be mockup data

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -2,51 +2,16 @@
 import { Line } from 'vue-chartjs'
 export default {
   extends: Line,
-  mounted () {
-    this.renderChart(
-      {
-        labels: [
-          'day 2',
-          'day 3',
-          'day 4',
-          'day 5',
-          'day 6',
-          'day 7',
-          'day 8'
-        ],
-        datasets: [
-          {
-            label: 'max',
-            data: [2, 10, 5, 9, 0, 6, 20],
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(255, 0,0, 0.35)',
-            pointBackgroundColor: 'rgba(255, 0, 0)'
-          },
-          {
-            label: 'min',
-            data: [10, 20, 30, 39, 10, 3, 39],
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(0, 231, 255, 0.35)',
-            pointBackgroundColor: 'rgba(0, 231, 255)'
-          },
-          {
-            label: 'min',
-            data: [30, 2, 13, 8, 3, 48, 20],
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(300, 220, 124,0.35)',
-            pointBackgroundColor: 'rgba(300, 220, 124)'
-          }
-        ]
-      },
-      {
-        responsive: true,
-        maintainAspectRatio: true,
-        title: {
-          display: true,
-          text: 'temperature for 7 days'
-        }
-      }
-    )
+  props: {
+    data: {
+      type: Object,
+      default: null
+    }
+  },
+  watch: {
+    data (value) {
+      this.renderChart(value)
+    }
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div>
-      <LineChart class="chart"/>
+    <div class="chartContainer">
+      <LineChart :data="tempDataOneWeek" class="chart" width="330px" height="300px" />
     </div>
   </div>
 </template>
@@ -11,6 +11,340 @@ import LineChart from '../components/LineChart'
 export default {
   components: {
     LineChart
+  },
+  data () {
+    return {
+      tempRawOneWeek: [
+        {
+          status: 200,
+          data: [
+            {
+              id_device: '1',
+              type: 'temp',
+              value: {
+                avg: 12,
+                max: 23,
+                min: 21
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'humidity',
+              value: {
+                avg: 30,
+                max: 120,
+                min: 31
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pm_1',
+              value: {
+                avg: 30,
+                max: 24,
+                min: 12
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pm_10',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pm_25',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'wind_velocity',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pressure',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+
+            {
+              id_device: '1',
+              type: 'temp',
+              value: {
+                avg: 50,
+                max: 22,
+                min: 25
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'humidity',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pm_1',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pm_10',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pm_25',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'wind_velocity',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '1',
+              type: 'pressure',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'temp',
+              value: {
+                avg: 94,
+                max: 48,
+                min: 94
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'humidity',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pm_1',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pm_10',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pm_25',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'wind_velocity',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pressure',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-12-05T13:42:00Z'
+            },
+
+            {
+              id_device: '2',
+              type: 'temp',
+              value: {
+                avg: 23,
+                max: 62,
+                min: 41
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'humidity',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pm_1',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pm_10',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pm_25',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'wind_velocity',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            },
+            {
+              id_device: '2',
+              type: 'pressure',
+              value: {
+                avg: 20,
+                max: 20,
+                min: 20
+              },
+              timestamp: '2020-11-05T13:42:00Z'
+            }
+          ]
+        }
+      ],
+      tempDataOneWeek: null
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      console.log('yahoo')
+      this.tempDataOneWeek = {
+        labels: [
+          'day 1',
+          'day 2',
+          'day 3',
+          'day 4',
+          'day 5',
+          'yest',
+          'today'
+        ],
+        datasets: [
+          {
+            label: 'max',
+            data: this.fillDatasets(),
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255, 0,0, 0.35)',
+            pointBackgroundColor: 'rgba(255, 0, 0)'
+          }
+        ]
+      }
+    },
+    fillDatasets () {
+      const dataMock = []
+      for (let j = 0; j < this.tempRawOneWeek[0].data.length; j++) {
+        if (this.tempRawOneWeek[0].data[j].id_device === '1') {
+          if (this.tempRawOneWeek[0].data[j].type === 'temp') {
+            dataMock.push(this.tempRawOneWeek[0].data[j].value.avg)
+          }
+        }
+      }
+      console.log(dataMock)
+      return dataMock
+    }
   }
 }
 </script>
@@ -26,9 +360,10 @@ export default {
 @media screen and (max-width: 414px) {
   .container {
     width: 100vw;
-    & > .chart {
-      margin: 0 20px;
+    & > .chartContainer {
+      margin:0 auto;
     }
+
   }
 }
 </style>
