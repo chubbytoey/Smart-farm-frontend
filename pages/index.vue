108a62fb007dcdb05c5a07b61f17382a66a0fd38
chubbytoey <toey.sasikrang@gmail.com>
2020-12-14 08:58:10 +0700

    ADD log to see query route

--- pages/index.vue
+++ pages/index.vue
@@ -814,6 +814,7 @@ export default {
           this.fetchData(this.userId)
         })
       }).catch(err => console.log(err))
+    console.log(this.$route.query)
   },
   methods: {
     fillData () {
